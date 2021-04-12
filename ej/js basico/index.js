d422f560677102aa0f9c563487a9490e1b01e768
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-04-11 22:44:50 -0600

    asdad

---
+++ ej/js basico/index.js
@@ -0,0 +1,48 @@
+// Truthy y Falsy //
+//Ejemplos en los que Boolean devuelve Falso:
+//Boolean(0); //false
+//Boolean(null); //false
+//Boolean(NaN); //false
+//Boolean(undefined); //false
+//Boolean(false); //false
+//Boolean(""); //false
+
+//Ejemplos en los que Boolean devuelve verdadero:
+// Boolean(1); //true para 1 o cualquier número diferente de cero (0)
+// Boolean("a"); //true para cualquier caracter o espacio en blanco en el string
+// Boolean([]); //true aunque el array esté vacío
+// Boolean({}); //true aunque el objeto esté vacío
+// Boolean(function(){}); //Cualquier función es verdadera también
+
+
+//Operadores binarios: //
+// 3 + 2 //Suma
+// 50 - 10 // Resta
+// 10 * 20 //Multiplicación
+// 20 / 2 //División
+
+// "Diego " + "De Granda" //concatenación de strings
+
+//Operadores unitarios:
+// !false //negación de la negación = true
+
+//Operadores de asignación:
+// var a = 1; //Asignamos un valor a la variable
+
+//Operadores para comparar:
+// 3 == "3"; //Compara los valores y devuelve "true" en este caso
+
+// 3 === "3"; //Compara y valida los tipos y valores. Devuelve "falso" en este caso
+
+// 5 < 3 //Compara y valida si el 5 es menor a 3
+// 5 > 3 //Compara y valida si el 5 es mayor a 3
+// 5 <= 6 //Compara y valida si el 5 es menor o igual al 6
+// 5 >= 6 //Compara y valida si el 5 es mayor o igual al 6
+
+// a && b //Valida si ambas variables son verdaderas para que se cumpla la condición
+// a || b //Aquí se cumple la condición si alguna de las dos variables es verdadera
+
+// var edad = 40
+// edad++ //Incrementa el valor en 1
+
+// edad += 2 //Incrementa el valor por 2
